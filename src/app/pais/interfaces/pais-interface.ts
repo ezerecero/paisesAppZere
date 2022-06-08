3a1e0cae4cff6b4cd43725829e7c34bc8ac67b08
ezerecero <eduardo@zerecero.com>
2022-06-08 12:35:40 -0500

    Region ok, interface country actulizada, css animated

--- src/app/pais/interfaces/pais-interface.ts
+++ src/app/pais/interfaces/pais-interface.ts
@@ -1,67 +1,248 @@
 export interface Country {
     name:         Name;
-    tld:          string[];
+    tld?:         string[];
     cca2:         string;
-    ccn3:         string;
+    ccn3?:        string;
     cca3:         string;
-    cioc:         string;
-    independent:  boolean;
-    status:       string;
+    cioc?:        string;
+    independent?: boolean;
+    status:       Status;
     unMember:     boolean;
-    currencies:   Currencies;
+    currencies?:  Currencies;
     idd:          Idd;
-    capital:      string[];
+    capital?:     string[];
     altSpellings: string[];
-    region:       string;
-    subregion:    string;
-    languages:    Languages;
+    region:       Region;
+    subregion?:   string;
+    languages?:   { [key: string]: string };
     translations: { [key: string]: Translation };
     latlng:       number[];
     landlocked:   boolean;
-    borders:      string[];
+    borders?:     string[];
     area:         number;
-    demonyms:     Demonyms;
-    flag:         string;
+    demonyms?:    Demonyms;
+    flag?:        string;
     maps:         Maps;
     population:   number;
-    gini:         Gini;
-    fifa:         string;
+    gini?:        { [key: string]: number };
+    fifa?:        string;
     car:          Car;
     timezones:    string[];
-    continents:   string[];
+    continents:   Continent[];
     flags:        CoatOfArms;
     coatOfArms:   CoatOfArms;
-    startOfWeek:  string;
+    startOfWeek:  StartOfWeek;
     capitalInfo:  CapitalInfo;
-    postalCode:   PostalCode;
+    postalCode?:  PostalCode;
 }
 
 export interface CapitalInfo {
-    latlng: number[];
+    latlng?: number[];
 }
 
 export interface Car {
-    signs: string[];
-    side:  string;
+    signs?: string[];
+    side:   Side;
+}
+
+export enum Side {
+    Left = "left",
+    Right = "right",
 }
 
 export interface CoatOfArms {
-    png: string;
-    svg: string;
+    png?: string;
+    svg?: string;
+}
+
+export enum Continent {
+    Africa = "Africa",
+    Antarctica = "Antarctica",
+    Asia = "Asia",
+    Europe = "Europe",
+    NorthAmerica = "North America",
+    Oceania = "Oceania",
+    SouthAmerica = "South America",
 }
 
 export interface Currencies {
-    EUR: Eur;
+    DOP?: Aed;
+    GHS?: Aed;
+    EUR?: Aed;
+    KHR?: Aed;
+    USD?: Aed;
+    BHD?: Aed;
+    ILS?: Aed;
+    QAR?: Aed;
+    RON?: Aed;
+    MGA?: Aed;
+    NPR?: Aed;
+    GBP?: Aed;
+    JEP?: Aed;
+    UAH?: Aed;
+    DKK?: Aed;
+    MRU?: Aed;
+    LYD?: Aed;
+    TMT?: Aed;
+    AOA?: Aed;
+    SHP?: Aed;
+    AMD?: Aed;
+    THB?: Aed;
+    AZN?: Aed;
+    GNF?: Aed;
+    KYD?: Aed;
+    XCD?: Aed;
+    SLL?: Aed;
+    CNY?: Aed;
+    GEL?: Aed;
+    JPY?: Aed;
+    FKP?: Aed;
+    ERN?: Aed;
+    AUD?: Aed;
+    KID?: Aed;
+    ARS?: Aed;
+    PHP?: Aed;
+    BND?: Aed;
+    SGD?: Aed;
+    XOF?: Aed;
+    XAF?: Aed;
+    KGS?: Aed;
+    NAD?: Aed;
+    ZAR?: Aed;
+    KES?: Aed;
+    GIP?: Aed;
+    KZT?: Aed;
+    KRW?: Aed;
+    PKR?: Aed;
+    SOS?: Aed;
+    ZMW?: Aed;
+    MWK?: Aed;
+    SSP?: Aed;
+    MAD?: Aed;
+    TVD?: Aed;
+    NGN?: Aed;
+    ETB?: Aed;
+    CAD?: Aed;
+    CVE?: Aed;
+    DZD?: Aed;
+    PEN?: Aed;
+    PLN?: Aed;
+    VUV?: Aed;
+    BGN?: Aed;
+    IMP?: Aed;
+    TZS?: Aed;
+    NOK?: Aed;
+    GGP?: Aed;
+    UZS?: Aed;
+    TRY?: Aed;
+    FJD?: Aed;
+    BYN?: Aed;
+    GTQ?: Aed;
+    UYU?: Aed;
+    CKD?: Aed;
+    NZD?: Aed;
+    TWD?: Aed;
+    LSL?: Aed;
+    VES?: Aed;
+    LKR?: Aed;
+    LRD?: Aed;
+    CUC?: Aed;
+    CUP?: Aed;
+    KMF?: Aed;
+    KWD?: Aed;
+    BMD?: Aed;
+    EGP?: Aed;
+    JOD?: Aed;
+    PGK?: Aed;
+    SZL?: Aed;
+    VND?: Aed;
+    MNT?: Aed;
+    YER?: Aed;
+    MOP?: Aed;
+    BSD?: Aed;
+    SAR?: Aed;
+    LAK?: Aed;
+    MMK?: Aed;
+    HRK?: Aed;
+    BZD?: Aed;
+    MKD?: Aed;
+    RSD?: Aed;
+    SBD?: Aed;
+    LBP?: Aed;
+    IRR?: Aed;
+    BAM?: BAM;
+    HNL?: Aed;
+    TJS?: Aed;
+    CRC?: Aed;
+    MDL?: Aed;
+    CHF?: Aed;
+    CZK?: Aed;
+    BRL?: Aed;
+    BWP?: Aed;
+    BIF?: Aed;
+    GYD?: Aed;
+    SYP?: Aed;
+    XPF?: Aed;
+    HUF?: Aed;
+    WST?: Aed;
+    TND?: Aed;
+    JMD?: Aed;
+    KPW?: Aed;
+    COP?: Aed;
+    OMR?: Aed;
+    MZN?: Aed;
+    BOB?: Aed;
+    UGX?: Aed;
+    HTG?: Aed;
+    STN?: Aed;
+    INR?: Aed;
+    PYG?: Aed;
+    ANG?: Aed;
+    SDG?: BAM;
+    ISK?: Aed;
+    DJF?: Aed;
+    AED?: Aed;
+    IQD?: Aed;
+    ZWL?: Aed;
+    MXN?: Aed;
+    MUR?: Aed;
+    RUB?: Aed;
+    TOP?: Aed;
+    SRD?: Aed;
+    PAB?: Aed;
+    RWF?: Aed;
+    TTD?: Aed;
+    MYR?: Aed;
+    SEK?: Aed;
+    ALL?: Aed;
+    SCR?: Aed;
+    IDR?: Aed;
+    BDT?: Aed;
+    BBD?: Aed;
+    MVR?: Aed;
+    CDF?: Aed;
+    AWG?: Aed;
+    GMD?: Aed;
+    HKD?: Aed;
+    BTN?: Aed;
+    NIO?: Aed;
+    CLP?: Aed;
+    AFN?: Aed;
+    FOK?: Aed;
 }
 
-export interface Eur {
+export interface Aed {
     name:   string;
     symbol: string;
 }
 
+export interface BAM {
+    name: string;
+}
+
 export interface Demonyms {
-    eng: Eng;
-    fra: Eng;
+    eng:  Eng;
+    fra?: Eng;
 }
 
 export interface Eng {
@@ -69,17 +250,9 @@ export interface Eng {
     m: string;
 }
 
-export interface Gini {
-    "2018": number;
-}
-
 export interface Idd {
-    root:     string;
-    suffixes: string[];
-}
-
-export interface Languages {
-    spa: string;
+    root?:     string;
+    suffixes?: string[];
 }
 
 export interface Maps {
@@ -88,13 +261,9 @@ export interface Maps {
 }
 
 export interface Name {
-    common:     string;
-    official:   string;
-    nativeName: NativeName;
-}
-
-export interface NativeName {
-    spa: Translation;
+    common:      string;
+    official:    string;
+    nativeName?: { [key: string]: Translation };
 }
 
 export interface Translation {
@@ -104,5 +273,25 @@ export interface Translation {
 
 export interface PostalCode {
     format: string;
-    regex:  string;
+    regex?: string;
+}
+
+export enum Region {
+    Africa = "Africa",
+    Americas = "Americas",
+    Antarctic = "Antarctic",
+    Asia = "Asia",
+    Europe = "Europe",
+    Oceania = "Oceania",
+}
+
+export enum StartOfWeek {
+    Monday = "monday",
+    Sunday = "sunday",
+    Turday = "turday",
+}
+
+export enum Status {
+    OfficiallyAssigned = "officially-assigned",
+    UserAssigned = "user-assigned",
 }
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [ `
+
+  button {
+    margin-right: 5px;
+  }
+  
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+  regionActiva: string = '';
 
+  region: string = ''; 
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+
+  constructor(private pasiService: PaisService) { }
+
+  getClaseCSS(region: string): string{
+    return (region === this.regionActiva)
+              ? 'btn btn-primary'
+              : 'btn btn-outline-primary'
+  }
+
+  activarRegion(regionRecibida:string){
+    
+    if (regionRecibida === this.regionActiva) {
+      console.log('Nada que actualizar');
+      return;
+    }
+    
+    this.regionActiva = regionRecibida;
+
+    this.hayError = false;
+    this.region = regionRecibida;
+    this.paises = [];
+
+    this.pasiService.buscarRegion(this.region)
+      .subscribe( (paisesRespuesta) => {
+        console.log(paisesRespuesta);
+        this.paises = paisesRespuesta        
+
+      }, (err) => {
+        this.hayError = true;
+        this.paises = [];
+      })
+  }
+  
   ngOnInit(): void {
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    //TODO: crear sugerencias
   }
 
 }
